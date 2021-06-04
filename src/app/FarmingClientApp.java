ae16cd87a05046d7f4a741ef58cb4ef653720f8c
JaryJay <jay.jrjren@gmail.com>
2021-06-04 19:15:44 -0400

    Update FarmingClientApp.java

--- src/app/FarmingClientApp.java
+++ src/app/FarmingClientApp.java
@@ -1,14 +1,28 @@
 package app;
 
 import java.io.IOException;
+import java.util.Queue;
 
+import event.ctsevent.CTSEvent;
+import event.stcevent.STCEvent;
 import network.ClientSideEventSenderAndReceiver;
 
+/**
+ * The entry point of the client.
+ * 
+ * @author Jay
+ *
+ */
 public class FarmingClientApp {
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		// Start a new ClientSideEventSenderAndReceiver
-		new ClientSideEventSenderAndReceiver().run();
+		ClientSideEventSenderAndReceiver csesas = new ClientSideEventSenderAndReceiver();
+		csesas.run();
+		Queue<CTSEvent> sendEventBuffer = csesas.getSendEventBuffer();
+		Queue<STCEvent> receiveEventBuffer = csesas.getReceiveEventBuffer();
+		// Put events into sendEventBuffer to send them to the server.
+		// Check receiveEventBuffer periodically for events sent from the server.
 	}
 
 }
