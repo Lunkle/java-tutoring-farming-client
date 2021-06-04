8b8e916db0b4ed1b19a61a298218134923529b9e
Lunkle <bobbob.donny@gmail.com>
2021-06-04 12:18:35 -0400

    Initial commit

---
+++ src/app/FarmingClientApp.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class FarmingClientApp {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		System.out.println("Starting Java Tutoring farming client.");
+		System.out.println("Name of this computer: " + InetAddress.getLocalHost().getCanonicalHostName());
+		System.out.println("LAN IP of this computer: " + InetAddress.getLocalHost().getHostAddress());
+		System.out.println("Contacting Donny's server...");
+		try (Socket socket = new Socket("72.140.156.47", 45000)) {
+			System.out.println("Connection accepted");
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			while (true) {
+				Object o = in.readObject();
+			}
+		}
+	}
+
+}
