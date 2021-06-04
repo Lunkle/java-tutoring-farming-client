4b5bce720afea16a3750d0994ee8b989ae719f8c
JaryJay <47123100+JaryJay@users.noreply.github.com>
2021-06-04 16:16:02 -0400

    Update FarmingClient.java

--- src/network/FarmingClient.java
+++ src/network/FarmingClient.java
@@ -4,28 +4,34 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import event.ctsevent.CTSEvent;
+import event.stcevent.STCEvent;
 
 public class FarmingClient implements Runnable {
 
 	private boolean running;
-	private Queue<CTSEvent> eventBuffer;
+	private Queue<CTSEvent> toSendEventBuffer;
+	private Queue<STCEvent> receiveEventBuffer;
 	private Socket socket;
 
 	public FarmingClient() {
 		running = false;
-		eventBuffer = new LinkedList<>();
+		toSendEventBuffer = new LinkedList<>();
+		receiveEventBuffer = new LinkedList<>();
 	}
 
 	@Override
 	public void run() {
-		running = true;
 		try {
 			System.out.println("Contacting Donny's server...");
 			socket = new Socket("72.140.156.47", 45000);
+			socket.setSoTimeout(160);
 			System.out.println("Connection accepted.");
 		} catch (Exception e) {
 			System.err.println("Failed to connect to Donny's server.");
@@ -33,28 +39,57 @@ public class FarmingClient implements Runnable {
 			System.out.println("Shutting down client.");
 			return;
 		}
+		running = true;
+		ExecutorService threadPool = Executors.newFixedThreadPool(2);
+		threadPool.execute(() -> {
+			ObjectOutputStream out = null;
+			try {
+				out = new ObjectOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+			while (running) {
+				sendEvents(out);
+				Thread.yield();
+			}
+		});
+		threadPool.execute(() -> {
+			ObjectInputStream in = null;
+			try {
+				in = new ObjectInputStream(socket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+			while (running) {
+				Thread.yield();
+			}
+		});
+	}
 
-		ObjectInputStream in = null;
-		ObjectOutputStream out = null;
+	private void sendEvents(ObjectOutputStream out) {
+		if (!toSendEventBuffer.isEmpty()) {
+			CTSEvent event = toSendEventBuffer.poll();
+			try {
+				out.writeObject(event);
+				System.out.println("[Message sent]: " + event.getDescription());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
+	private void receiveEvents(ObjectInputStream in) {
+		byte[] bytes = new byte[1024];
 		try {
-			in = new ObjectInputStream(socket.getInputStream());
-			out = new ObjectOutputStream(socket.getOutputStream());
+			in.read(bytes);
+			// Deserialize byte array into a STCEvent
+			// Add STCEvent to receiveEventBuffer
+		} catch (SocketTimeoutException e) {
+			return;
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-
-		while (running) {
-			if (!eventBuffer.isEmpty()) {
-				CTSEvent event = eventBuffer.poll();
-				try {
-					out.writeObject(event);
-					System.out.println("[Message sent]: " + event.getDescription());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			Thread.yield();
 		}
 	}
 
