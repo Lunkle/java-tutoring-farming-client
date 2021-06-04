ab393b921924d682f9c758fa173649f95acfa12e
JaryJay <jay.jrjren@gmail.com>
2021-06-04 14:27:55 -0400

    Update FarmingClientApp.java

--- src/app/FarmingClientApp.java
+++ src/app/FarmingClientApp.java
@@ -12,12 +12,15 @@ public class FarmingClientApp {
 		System.out.println("Name of this computer: " + InetAddress.getLocalHost().getCanonicalHostName());
 		System.out.println("LAN IP of this computer: " + InetAddress.getLocalHost().getHostAddress());
 		System.out.println("Contacting Donny's server...");
+		boolean running = true;
 		try (Socket socket = new Socket("72.140.156.47", 45000)) {
 			System.out.println("Connection accepted");
 			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-			while (true) {
+			while (running) {
 				Object o = in.readObject();
+				System.out.println("Object received: " + o);
 			}
+			in.close();
 		}
 	}
 
